f1d39b9ed2a69ee467b51c6804ed5ed3b1d59dac
Saed Yousef <saed.alzaben@gmail.com>
2020-05-03 00:56:14 +0300

    Success build

--- flights/tests.py
+++ flights/tests.py
@@ -22,7 +22,7 @@ class ModelsTestCase(TestCase):
 
     def test_arrivals_count(self):
         a = Airport.objects.get(code="AAA")
-        self.assertEqual(a.arrivals.count(), 2)
+        self.assertEqual(a.arrivals.count(), 1)
 
     def test_valid_flight(self):
         a1 = Airport.objects.get(code="AAA")
