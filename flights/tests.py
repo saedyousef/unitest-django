572ff8ba57dec88a4286db5eefd5a17556ce03f6
Saed Yousef <saed.alzaben@gmail.com>
2020-05-03 00:53:48 +0300

    Test fail build

--- flights/tests.py
+++ flights/tests.py
@@ -22,14 +22,14 @@ class ModelsTestCase(TestCase):
 
     def test_arrivals_count(self):
         a = Airport.objects.get(code="AAA")
-        self.assertEqual(a.arrivals.count(), 1)
-    
+        self.assertEqual(a.arrivals.count(), 2)
+
     def test_valid_flight(self):
         a1 = Airport.objects.get(code="AAA")
         a2 = Airport.objects.get(code="BBB")
         f = Flight.objects.get(origin=a1, destination=a2, duration=100)
         self.assertTrue(f.is_valid_flight())
-        
+
     def test_invalid_flight_destination(self):
         a1 = Airport.objects.get(code="AAA")
         f = Flight.objects.get(origin=a1, destination=a1)
@@ -47,7 +47,7 @@ class ModelsTestCase(TestCase):
         response = c.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['flights'].count(), 2)
-  
+
     def test_valid_flight(self):
         a1 = Airport.objects.get(code="AAA")
         f = Flight.objects.get(origin=a1, destination=a1)
